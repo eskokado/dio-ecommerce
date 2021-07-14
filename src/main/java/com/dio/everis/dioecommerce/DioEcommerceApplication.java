b028f3661334604ece745be2eaf682e534a56c4f
Edson Shideki Kokado <eskokado@gmail.com>
2021-07-14 07:18:18 -0300

    chore:initial commit

---
+++ src/main/java/com/dio/everis/dioecommerce/DioEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.dio.everis.dioecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/everis/dioecommerce/DioEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.everis.dioecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
